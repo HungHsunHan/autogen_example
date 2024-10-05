39a45f6a8222581b0f783c3ec4b332dc0701d646
HungHsunHan <110224143+HungHsunHan@users.noreply.github.com>
2024-10-05 10:24:01 +0800

    Use docker env

--- main.py
+++ main.py
@@ -55,7 +55,7 @@ user_proxy = autogen.UserProxyAgent(
     is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
     code_execution_config={
         "work_dir": "web",
-        "use_docker": False,
+        "use_docker": True,
     },  # Please set use_docker=True if docker is available to run the generated code. Using docker is safer than running the generated code directly.
     llm_config=llm_config,
     system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
